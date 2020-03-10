8e8fe49f23b0de9a3f54d51f7df59f2b5feb18b5
stansss <810583354@qq.com>
2020-03-10 18:16:21 +0800

    用户信息切换为MyUserDetails

---
+++ src/main/java/com/stan/server/model/MyUserDetails.java
@@ -0,0 +1,263 @@
+package com.stan.server.model;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.security.core.CredentialsContainer;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.util.Assert;
+
+import java.io.Serializable;
+import java.util.*;
+import java.util.function.Function;
+
+public class MyUserDetails implements UserDetails, CredentialsContainer {
+    private static final long serialVersionUID = 520L;
+    private static final Log logger = LogFactory.getLog(User.class);
+    private String password;
+    private final String username;
+    private final Integer userId;
+    private final Set<GrantedAuthority> authorities;
+    private final boolean accountNonExpired;
+    private final boolean accountNonLocked;
+    private final boolean credentialsNonExpired;
+    private final boolean enabled;
+
+    public MyUserDetails(Integer userId, String username, String password, Collection<? extends GrantedAuthority> authorities) {
+        this(userId, username, password, true, true, true, true, authorities);
+    }
+
+    public MyUserDetails(Integer userId, String username, String password, boolean enabled, boolean accountNonExpired, boolean credentialsNonExpired, boolean accountNonLocked, Collection<? extends GrantedAuthority> authorities) {
+        if (userId != null && username != null && !"".equals(username) && password != null) {
+            this.userId = userId;
+            this.username = username;
+            this.password = password;
+            this.enabled = enabled;
+            this.accountNonExpired = accountNonExpired;
+            this.credentialsNonExpired = credentialsNonExpired;
+            this.accountNonLocked = accountNonLocked;
+            this.authorities = Collections.unmodifiableSet(sortAuthorities(authorities));
+        } else {
+            throw new IllegalArgumentException("Cannot pass null or empty values to constructor");
+        }
+    }
+
+    public Collection<GrantedAuthority> getAuthorities() {
+        return this.authorities;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public boolean isEnabled() {
+        return this.enabled;
+    }
+
+    public boolean isAccountNonExpired() {
+        return this.accountNonExpired;
+    }
+
+    public boolean isAccountNonLocked() {
+        return this.accountNonLocked;
+    }
+
+    public boolean isCredentialsNonExpired() {
+        return this.credentialsNonExpired;
+    }
+
+    public void eraseCredentials() {
+        this.password = null;
+    }
+
+    private static SortedSet<GrantedAuthority> sortAuthorities(Collection<? extends GrantedAuthority> authorities) {
+        Assert.notNull(authorities, "Cannot pass a null GrantedAuthority collection");
+        SortedSet<GrantedAuthority> sortedAuthorities = new TreeSet(new MyUserDetails.AuthorityComparator());
+        Iterator var2 = authorities.iterator();
+
+        while(var2.hasNext()) {
+            GrantedAuthority grantedAuthority = (GrantedAuthority)var2.next();
+            Assert.notNull(grantedAuthority, "GrantedAuthority list cannot contain any null elements");
+            sortedAuthorities.add(grantedAuthority);
+        }
+
+        return sortedAuthorities;
+    }
+
+    public boolean equals(Object rhs) {
+        return rhs instanceof User ? this.username.equals(((MyUserDetails)rhs).username) : false;
+    }
+
+    public int hashCode() {
+        return this.username.hashCode();
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(super.toString()).append(": ");
+        sb.append("Username: ").append(this.username).append("; ");
+        sb.append("Password: [PROTECTED]; ");
+        sb.append("Enabled: ").append(this.enabled).append("; ");
+        sb.append("AccountNonExpired: ").append(this.accountNonExpired).append("; ");
+        sb.append("credentialsNonExpired: ").append(this.credentialsNonExpired).append("; ");
+        sb.append("AccountNonLocked: ").append(this.accountNonLocked).append("; ");
+        if (!this.authorities.isEmpty()) {
+            sb.append("Granted Authorities: ");
+            boolean first = true;
+            Iterator var3 = this.authorities.iterator();
+
+            while(var3.hasNext()) {
+                GrantedAuthority auth = (GrantedAuthority)var3.next();
+                if (!first) {
+                    sb.append(",");
+                }
+
+                first = false;
+                sb.append(auth);
+            }
+        } else {
+            sb.append("Not granted any authorities");
+        }
+
+        return sb.toString();
+    }
+
+    public static MyUserDetails.UserBuilder withUsername(String username) {
+        return builder().username(username);
+    }
+
+    public static MyUserDetails.UserBuilder builder() {
+        return new MyUserDetails.UserBuilder();
+    }
+
+    /** @deprecated */
+    @Deprecated
+    public static MyUserDetails.UserBuilder withDefaultPasswordEncoder() {
+        logger.warn("User.withDefaultPasswordEncoder() is considered unsafe for production and is only intended for sample applications.");
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        MyUserDetails.UserBuilder var10000 = builder();
+        encoder.getClass();
+        return var10000.passwordEncoder(encoder::encode);
+    }
+
+    public static MyUserDetails.UserBuilder withUserDetails(UserDetails userDetails) {
+        return withUsername(userDetails.getUsername()).password(userDetails.getPassword()).accountExpired(!userDetails.isAccountNonExpired()).accountLocked(!userDetails.isAccountNonLocked()).authorities(userDetails.getAuthorities()).credentialsExpired(!userDetails.isCredentialsNonExpired()).disabled(!userDetails.isEnabled());
+    }
+
+    public static class UserBuilder {
+        private String username;
+        private String password;
+        private List<GrantedAuthority> authorities;
+        private boolean accountExpired;
+        private boolean accountLocked;
+        private boolean credentialsExpired;
+        private boolean disabled;
+        private Function<String, String> passwordEncoder;
+
+        private UserBuilder() {
+            this.passwordEncoder = (password) -> {
+                return password;
+            };
+        }
+
+        public MyUserDetails.UserBuilder username(String username) {
+            Assert.notNull(username, "username cannot be null");
+            this.username = username;
+            return this;
+        }
+
+        public MyUserDetails.UserBuilder password(String password) {
+            Assert.notNull(password, "password cannot be null");
+            this.password = password;
+            return this;
+        }
+
+        public MyUserDetails.UserBuilder passwordEncoder(Function<String, String> encoder) {
+            Assert.notNull(encoder, "encoder cannot be null");
+            this.passwordEncoder = encoder;
+            return this;
+        }
+
+        public MyUserDetails.UserBuilder roles(String... roles) {
+            List<GrantedAuthority> authorities = new ArrayList(roles.length);
+            String[] var3 = roles;
+            int var4 = roles.length;
+
+            for(int var5 = 0; var5 < var4; ++var5) {
+                String role = var3[var5];
+                Assert.isTrue(!role.startsWith("ROLE_"), () -> {
+                    return role + " cannot start with ROLE_ (it is automatically added)";
+                });
+                authorities.add(new SimpleGrantedAuthority("ROLE_" + role));
+            }
+
+            return this.authorities((Collection)authorities);
+        }
+
+        public MyUserDetails.UserBuilder authorities(GrantedAuthority... authorities) {
+            return this.authorities((Collection)Arrays.asList(authorities));
+        }
+
+        public MyUserDetails.UserBuilder authorities(Collection<? extends GrantedAuthority> authorities) {
+            this.authorities = new ArrayList(authorities);
+            return this;
+        }
+
+        public MyUserDetails.UserBuilder authorities(String... authorities) {
+            return this.authorities((Collection) AuthorityUtils.createAuthorityList(authorities));
+        }
+
+        public MyUserDetails.UserBuilder accountExpired(boolean accountExpired) {
+            this.accountExpired = accountExpired;
+            return this;
+        }
+
+        public MyUserDetails.UserBuilder accountLocked(boolean accountLocked) {
+            this.accountLocked = accountLocked;
+            return this;
+        }
+
+        public MyUserDetails.UserBuilder credentialsExpired(boolean credentialsExpired) {
+            this.credentialsExpired = credentialsExpired;
+            return this;
+        }
+
+        public MyUserDetails.UserBuilder disabled(boolean disabled) {
+            this.disabled = disabled;
+            return this;
+        }
+
+        public UserDetails build() {
+            String encodedPassword = (String)this.passwordEncoder.apply(this.password);
+            return new User(this.username, encodedPassword, !this.disabled, !this.accountExpired, !this.credentialsExpired, !this.accountLocked, this.authorities);
+        }
+    }
+
+    private static class AuthorityComparator implements Comparator<GrantedAuthority>, Serializable {
+        private static final long serialVersionUID = 520L;
+
+        private AuthorityComparator() {
+        }
+
+        public int compare(GrantedAuthority g1, GrantedAuthority g2) {
+            if (g2.getAuthority() == null) {
+                return -1;
+            } else {
+                return g1.getAuthority() == null ? 1 : g1.getAuthority().compareTo(g2.getAuthority());
+            }
+        }
+    }
+}
