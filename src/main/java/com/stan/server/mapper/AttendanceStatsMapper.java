82ca7a8e2c27893b06e24cb6497f4fa69fc82436
rynar <2287220381@qq.com>
2020-05-04 20:29:39 +0800

    bb

---
+++ src/main/java/com/stan/server/mapper/AttendanceStatsMapper.java
@@ -0,0 +1,16 @@
+package com.stan.server.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.stan.server.entity.AttendanceStats;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author Ren
+ * @since 2020-05-04
+ */
+public interface AttendanceStatsMapper extends BaseMapper<AttendanceStats> {
+
+}
