bf7c1e76bfb0aa3e49494bab22d32c3fb979a09c
rynar <2287220381@qq.com>
2020-05-04 22:48:42 +0800

    bb

--- src/main/java/com/stan/server/controller/AttendanceRulesController.java
+++ src/main/java/com/stan/server/controller/AttendanceRulesController.java
@@ -5,7 +5,6 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.stan.server.entity.AttendanceRules;
-import com.stan.server.enums.AttendanceModeEnum;
 import com.stan.server.service.AttendanceRulesService;
 import com.stan.server.service.AttendanceRulesTimeService;
 import com.stan.server.utils.ResultVO;
